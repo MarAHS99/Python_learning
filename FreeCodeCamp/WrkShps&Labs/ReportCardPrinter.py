d13a20305106a40c6a9f5b37f885a5058f8ed99e
MarAHS99 <aguirre.marcelo.ext@gmail.com>
2026-03-08 17:57:07 -0300

    Create ReportCardPrinter.py

---
+++ FreeCodeCamp/WrkShps&Labs/ReportCardPrinter.py
@@ -0,0 +1,12 @@
+name = 'Marcelo'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 26
+print(age, type(age))
+
+score = 92.4
+print(isinstance(score, float))
+print(score,type(score))
